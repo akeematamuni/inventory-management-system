fb7d600cae7363415bca4e1dd61d1b3b4663b096
akeematamuni <amuni.engr@gmail.com>
2026-03-06 07:59:37 -0500

    Inventory management project bootstrap

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,21 +1,21 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { config } from 'dotenv';
+
+config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-  );
+    const app = await NestFactory.create(AppModule);
+    const logger = new Logger('Bootstrap');
+    
+    const globalPrefix = process.env.GLOBAL_PREFIX || 'api/v1';
+    const port = process.env.PORT || 3000;
+
+    app.setGlobalPrefix(globalPrefix);
+    await app.listen(port);
+
+    logger.log(`Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
 
 bootstrap();
