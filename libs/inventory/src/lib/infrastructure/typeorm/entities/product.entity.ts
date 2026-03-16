24692f61bb56fc3ba2c9d84a3f448f34072852b9
akeematamuni <amuni.engr@gmail.com>
2026-03-16 11:06:25 -0400

    feat (inventory): infrastructure entities definition

---
+++ libs/inventory/src/lib/infrastructure/typeorm/entities/product.entity.ts
@@ -0,0 +1,38 @@
+import { Entity, Column, PrimaryColumn, Index, CreateDateColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('products')
+export class ProductEntityTypeOrm {
+    @PrimaryColumn('uuid')
+    id!: string;
+
+    @Column({ type: 'varchar', length: 100 })
+    name!: string;
+
+    @Index()
+    @Column({ type: 'varchar', length: 20, unique: true})
+    sku!: string;
+
+    @Column({ type: 'text', nullable: true})
+    description?: string | null;
+
+    @Column({ name: 'unit_cost', type: 'decimal', precision: 15, scale: 4, default: 0 })
+    unitCost!: number;
+
+    @Column({ type: 'varchar', length: 3, default: 'USD' })
+    currency!: string;
+
+    @Column({ name: 'reorder_point', type: 'int', default: 0 })
+    reorderPoint!: number;
+
+    @Column({ type: 'varchar', length: 150, unique: true, nullable: true })
+    barcode!: string;
+
+    @Column({ name: 'is_active', type: 'boolean', default: true })
+    isActive!: boolean
+
+    @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
+    createdAt!: Date;
+    
+    @UpdateDateColumn({ name: 'updated_at', type: 'timestamp' })
+    updatedAt!: Date;
+}
