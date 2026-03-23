c07559841d03bdcd121b5f3db7b7061065474a8d
akeematamuni <amuni.engr@gmail.com>
2026-03-23 08:55:56 -0400

    feat (inventory): product and warehouse to move to a document db

--- libs/inventory/src/lib/infrastructure/typeorm/entities/product.entity.ts
+++ libs/inventory/src/lib/infrastructure/typeorm/entities/product.entity.ts
@@ -1,38 +1,38 @@
-import { Entity, Column, PrimaryColumn, Index, CreateDateColumn, UpdateDateColumn } from "typeorm";
+// import { Entity, Column, PrimaryColumn, Index, CreateDateColumn, UpdateDateColumn } from "typeorm";
 
-@Entity('products')
-export class ProductEntityTypeOrm {
-    @PrimaryColumn('uuid')
-    id!: string;
+// @Entity('products')
+// export class ProductEntityTypeOrm {
+//     @PrimaryColumn('uuid')
+//     id!: string;
 
-    @Column({ type: 'varchar', length: 100 })
-    name!: string;
+//     @Column({ type: 'varchar', length: 100 })
+//     name!: string;
 
-    @Index()
-    @Column({ type: 'varchar', length: 20, unique: true})
-    sku!: string;
+//     @Index()
+//     @Column({ type: 'varchar', length: 20, unique: true})
+//     sku!: string;
 
-    @Column({ type: 'text', nullable: true})
-    description?: string | null;
+//     @Column({ type: 'text', nullable: true})
+//     description?: string | null;
 
-    @Column({ name: 'unit_cost', type: 'decimal', precision: 15, scale: 4, default: 0 })
-    unitCost!: string;
+//     @Column({ name: 'unit_cost', type: 'decimal', precision: 15, scale: 4, default: 0 })
+//     unitCost!: string;
 
-    @Column({ type: 'varchar', length: 3, default: 'USD' })
-    currency!: string;
+//     @Column({ type: 'varchar', length: 3, default: 'USD' })
+//     currency!: string;
 
-    @Column({ name: 'reorder_point', type: 'int', default: 0 })
-    reorderPoint!: number;
+//     @Column({ name: 'reorder_point', type: 'int', default: 0 })
+//     reorderPoint!: number;
 
-    @Column({ type: 'varchar', length: 150, unique: true, nullable: true })
-    barcode?: string | null;
+//     @Column({ type: 'varchar', length: 150, unique: true, nullable: true })
+//     barcode?: string | null;
 
-    @Column({ name: 'is_active', type: 'boolean', default: true })
-    isActive!: boolean
+//     @Column({ name: 'is_active', type: 'boolean', default: true })
+//     isActive!: boolean
 
-    @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
-    createdAt!: Date;
+//     @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
+//     createdAt!: Date;
     
-    @UpdateDateColumn({ name: 'updated_at', type: 'timestamp' })
-    updatedAt!: Date;
-}
+//     @UpdateDateColumn({ name: 'updated_at', type: 'timestamp' })
+//     updatedAt!: Date;
+// }
--- libs/inventory/src/lib/infrastructure/typeorm/entities/warehouse.entity.ts
+++ libs/inventory/src/lib/infrastructure/typeorm/entities/warehouse.entity.ts
@@ -1,26 +1,26 @@
-import { Entity, Column, PrimaryColumn, CreateDateColumn, UpdateDateColumn, Index } from 'typeorm';
+// import { Entity, Column, PrimaryColumn, CreateDateColumn, UpdateDateColumn, Index } from 'typeorm';
 
-@Entity('warehouses')
-export class WarehouseEntityTypeOrm {
-    @PrimaryColumn('uuid')
-    id!: string;
+// @Entity('warehouses')
+// export class WarehouseEntityTypeOrm {
+//     @PrimaryColumn('uuid')
+//     id!: string;
 
-    @Column({ type: 'varchar', length: 50 })
-    name!: string;
+//     @Column({ type: 'varchar', length: 50 })
+//     name!: string;
 
-    @Index()
-    @Column({ type: 'varchar', length: 10, unique: true })
-    code!: string;
+//     @Index()
+//     @Column({ type: 'varchar', length: 10, unique: true })
+//     code!: string;
 
-    @Column({ type: 'text', nullable: true })
-    address?: string | null;
+//     @Column({ type: 'text', nullable: true })
+//     address?: string | null;
     
-    @Column({ name: 'is_active', type: 'boolean', default: true })
-    isActive!: boolean;
+//     @Column({ name: 'is_active', type: 'boolean', default: true })
+//     isActive!: boolean;
     
-    @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
-    createdAt!: Date;
+//     @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
+//     createdAt!: Date;
     
-    @UpdateDateColumn({ name: 'updated_at', type: 'timestamp' })
-    updatedAt!: Date;
-}
+//     @UpdateDateColumn({ name: 'updated_at', type: 'timestamp' })
+//     updatedAt!: Date;
+// }
