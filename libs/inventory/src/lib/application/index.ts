1ee589b6713061a582ff3e78e2f73052f8ee18d8
akeematamuni <amuni.engr@gmail.com>
2026-03-31 10:19:41 -0400

    feat (inventory): barrel exports for application layer

---
+++ libs/inventory/src/lib/application/index.ts
@@ -0,0 +1,46 @@
+// DTOs
+export * from './dtos/warehouse.dto';
+export * from './dtos/product.dto';
+export * from './dtos/purchasing.dto';
+export * from './dtos/transfer.dto';
+
+// Warehouse
+export * from './command/warehouse/create-warehouse.command';
+export * from './command/warehouse/create-warehouse.handler';
+export * from './command/warehouse/deactivate-warehouse.command';
+export * from './command/warehouse/deactivate-warehouse.handler';
+export * from './command/warehouse/update-warehouse.command';
+export * from './command/warehouse/update-warehouse.handler';
+export * from './query/warehouse/get-warehouse.query';
+export * from './query/warehouse/get-warehouse.handler';
+export * from './query/warehouse/get-all-warehouses.query';
+export * from './query/warehouse/get-all-warehouses.handler';
+
+// Product
+export * from './command/product/activate-product.command';
+export * from './command/product/activate-product.handler';
+export * from './command/product/create-product.command';
+export * from './command/product/create-product.handler';
+export * from './command/product/deactivate-product.command';
+export * from './command/product/deactivate-product.handler';
+export * from './command/product/update-product.command';
+export * from './command/product/update-product.handler'
+export * from './query/product/get-product.query';
+export * from './query/product/get-product.handler';
+export * from './query/product/get-all-product.query';
+export * from './query/product/get-all-product.handler';
+
+// Purchasing
+export * from './command/purchasing/confirm-goods-receipt.command';
+export * from './command/purchasing/confirm-goods-reciept.handler';
+export * from './command/purchasing/confirm-purchase-order.command';
+export * from './command/purchasing/confirm-purchase-order.handler';
+export * from './command/purchasing/create-purchase-order.command';
+export * from './command/purchasing/create-purchase-order.command';
+export * from './command/purchasing/create-purchase-order.handler';
+export * from './query/purchasing/get-purchase-order.query';
+export * from './query/purchasing/get-purchase-order.handler';
+export * from './query/purchasing/get-all-purchase-orders.query';
+export * from './query/purchasing/get-all-purchase-orders.handler';
+
+// Transfer
