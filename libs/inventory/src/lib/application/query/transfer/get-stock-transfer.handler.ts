0911e52d7a109e4f6bd1dea3a410299cd6510a00
akeematamuni <amuni.engr@gmail.com>
2026-04-01 06:10:54 -0400

    feat (inventory): getting stock tranfer between warehouses' query and handlr

---
+++ libs/inventory/src/lib/application/query/transfer/get-stock-transfer.handler.ts
@@ -0,0 +1,24 @@
+import { QueryHandler, IQueryHandler } from "@nestjs/cqrs";
+import { Inject } from "@nestjs/common";
+
+import {
+    IStockTransferRepository, STOCK_TRANSFER_REPOSITORY,
+    StockTransferNotFoundException
+} from "../../../domain";
+
+import { GetStockTransferQuery } from "./get-stock-transfer.query";
+import { StockTransferResponseDto } from "../../dtos/transfer.dto";
+
+@QueryHandler(GetStockTransferQuery)
+export class GetStockTransferHandler implements IQueryHandler<GetStockTransferQuery> {
+    constructor(
+        @Inject(STOCK_TRANSFER_REPOSITORY)
+        private readonly transferRepo: IStockTransferRepository
+    ) {}
+
+    async execute(query: GetStockTransferQuery): Promise<StockTransferResponseDto> {
+        const transfer = await this.transferRepo.findById(query.id);
+        if (!transfer) throw new StockTransferNotFoundException(query.id);
+        return StockTransferResponseDto.fromDomain(transfer);
+    }
+}
