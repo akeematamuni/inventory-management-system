840cacb161a28a283a8311b3517128fcab7253da
akeematamuni <amuni.engr@gmail.com>
2026-04-07 06:19:18 -0400

    feat (core): creation of current user decorator

--- libs/core/src/lib/decorators/index.ts
+++ libs/core/src/lib/decorators/index.ts
@@ -2,3 +2,4 @@ export * from './public.decorator';
 export * from './manual-body.decorator';
 export * from './current-user-email.decorator';
 export * from './kafka-payload.decorator';
+export * from './current-user-id.decorator';
