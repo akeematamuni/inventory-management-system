250f7077cf7c918a5b558a52ec0ba4170cfe9a9f
akeematamuni <amuni.engr@gmail.com>
2026-04-08 05:51:13 -0400

    feat (core): added mongo uri to zod for parsing

--- libs/core/src/lib/config/config.schema.ts
+++ libs/core/src/lib/config/config.schema.ts
@@ -8,9 +8,10 @@ export const configSchema = z.object({
     JWT_EXPIRES: z.coerce.string().default('15m'),
     
     JWT_REFRESH_SECRET: z.coerce.string().min(32),
-    JWT_REFRESH_EXPIRES: z.coerce.string().default('24h'),
+    JWT_REFRESH_EXPIRES: z.coerce.string().default('48h'),
 
     DATABASE_URL: z.coerce.string(),
+    MONGO_URI: z.coerce.string(),
 
     NODE_ENV: z.coerce.string().default('production')
 });
