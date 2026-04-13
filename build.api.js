ba65be3cd7f52ab089171f6800d2e361c1b92da2
akeematamuni <amuni.engr@gmail.com>
2026-04-13 11:23:26 -0400

    feat (system): manual build script for api and dependencies update

---
+++ build.api.js
@@ -0,0 +1,53 @@
+/** @type {import('webpack').Configuration} */
+
+const path = require('path');
+const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+const TsConfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+const workspaceRoot = path.resolve(__dirname);
+
+const config = {
+    target: 'node',
+    mode: 'production',
+    entry: path.join(workspaceRoot, 'apps/api/src/main.ts'),
+    output: {
+        path: path.join(workspaceRoot, 'dist/apps/api'),
+        filename: 'main.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+                options: {
+                    configFile: path.join(workspaceRoot, 'apps/api/tsconfig.app.json'),
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+        plugins: [new TsConfigPathsPlugin({
+            configFile: path.join(workspaceRoot, 'tsconfig.base.json')
+        })]
+    },
+    externals: [nodeExternals()], 
+};
+
+webpack(config, (err, stats) => {
+    if (err) {
+        console.error('FATAL ERROR:', err);
+        process.exit(1);
+    }
+
+    const info = stats.toJson();
+
+    if (stats.hasErrors()) {
+        console.error('COMPILATION ERRORS:', info.errors);
+        process.exit(1);
+    }
+
+    console.log('Successfully built inventory API app');
+});
