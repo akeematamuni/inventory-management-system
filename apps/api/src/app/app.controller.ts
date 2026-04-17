89eff475018596d88edc7e0e4a36d0b36525cc2a
akeematamuni <amuni.engr@gmail.com>
2026-04-17 08:17:29 -0400

    feat (app): health check route made public

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { Public } from '@inventory/core/decorators';
 
 @ApiTags('Health')
 @Controller('health')
 export class AppHealthController {
+    @Public()
     @Get()
     @ApiOperation({ summary: 'System health check' })
     check() {
